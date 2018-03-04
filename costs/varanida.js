93482d97e20fce3bdc0bbc3b85bdc6f1873161d0
marcvincenti <mvincenti@umi.us>
2018-03-04 11:43:54 +0100

    add all differents costs of valid mint function execution

--- costs/varanida.js
+++ costs/varanida.js
@@ -1,8 +1,9 @@
 'use strict';
 
 var Varanida = artifacts.require("./Varanida.sol");
+const Time = require("../test/helpers/time.js");
 
-contract('Varanida\n', function(accounts) {
+contract('Varanida - Mint()\n', function(accounts) {
 
   const owner = accounts[0],
   random_guy = accounts[1],
@@ -19,6 +20,36 @@ contract('Varanida\n', function(accounts) {
     }).then(function(result) {
       var gas = Number(result);
       console.log(" (mint) gas estimation = " + gas + " units"); // 99958
+      console.log(" (mint) gas cost estimation = " + Varanida.web3.fromWei((gas * gasPrice), 'ether') + " ether\n");
+    });
+  });
+
+  it("mint 2th VAD in the same day", function() {
+    var varanida;
+    return Varanida.deployed().then(function(instance) {
+      varanida = instance;
+      return varanida.mint(random_guy, amount, {from: owner});
+    }).then(function() {
+      return varanida.mint.estimateGas(random_guy, amount);
+    }).then(function(result) {
+      var gas = Number(result);
+      console.log(" (mint) gas estimation = " + gas + " units"); // 44738
+      console.log(" (mint) gas cost estimation = " + Varanida.web3.fromWei((gas * gasPrice), 'ether') + " ether\n");
+    });
+  });
+
+  it("mint 2th VAD on another day", function() {
+    var varanida;
+    return Varanida.deployed().then(function(instance) {
+      varanida = instance;
+      return varanida.mint(random_guy, amount, {from: owner});
+    }).then(function(){
+      return Time.increaseTime(60*60*24);
+    }).then(function() {
+      return varanida.mint.estimateGas(random_guy, amount);
+    }).then(function(result) {
+      var gas = Number(result);
+      console.log(" (mint) gas estimation = " + gas + " units"); // 54958
       console.log(" (mint) gas cost estimation = " + Varanida.web3.fromWei((gas * gasPrice), 'ether') + " ether\n");
     });
   });
